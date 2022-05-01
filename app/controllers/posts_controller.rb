43e906cb11107d8759509619745214a84eceb569
yoshitogotoda <yoshito.gotoda@gmail.com>
2022-05-01 18:47:58 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,19 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
 
-  
-
   def create
     Post.create(content: params[:content])
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item}
   end
 end
