9e051858c27ce22380c41016c665302b30d4d916
yoshitogotoda <yoshito.gotoda@gmail.com>
2022-05-01 15:58:52 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
+  
 
   def create
     Post.create(content: params[:content])
