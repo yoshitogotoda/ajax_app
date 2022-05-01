28af87ab3b7f7067e375b4a9e602f9ba2ae0ceb9
yoshitogotoda <yoshito.gotoda@gmail.com>
2022-05-01 23:00:36 +0900

    メモ投稿機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  post 'posts', to: 'posts#create'
+  resources :posts,only: [:create,:destroy]
   get 'posts/:id', to: 'posts#checked'
 end
